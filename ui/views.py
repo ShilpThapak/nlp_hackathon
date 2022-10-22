e2c3dd29df7b4243fdc39d0fc8f2e82db29031bc
ShilpThapak <thapak.shilp2395@gmail.com>
2022-10-22 21:10:22 +0530

    Added sentiment analysis

--- ui/views.py
+++ ui/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 import io
+import os
+from expertai.nlapi.cloud.client import ExpertAiClient
+
 
 # Create your views here.
 def index(request):
@@ -19,5 +22,27 @@ def dashboard(request):
         
     uploaded_file = request.FILES['document']
     df = pd.read_csv(io.BytesIO(uploaded_file.read()))
+    if len(df.columns) != 1:
+        message = "Please upload a file with one column only!"
+        return render(request,'ui/index.html', {"message": message})
+    df.columns = ["text"]
+    df = sentiment(df)
     data = df.to_dict('split')
     return render(request,'ui/dashboard.html', {"data": data})
+
+def sentiment(df):
+    os.environ["EAI_USERNAME"] = 'sudurima.banerjee@rakuten.com'
+    os.environ["EAI_PASSWORD"] = 'Rima@280'
+    client = ExpertAiClient()
+
+    list = []
+    for i in range(0,len(df)):
+        text=df['text'].iloc[i]
+        language= 'en'
+        document = client.specific_resource_analysis(
+        body={"document": {"text": text}}, 
+        params={'language': language, 'resource': 'sentiment'})
+        list.append(document.sentiment.overall)
+
+    df['Sentiment score'] = list
+    return df
