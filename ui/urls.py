91524c31cf59f56086cc70da6dec52107a2336dd
ShilpThapak <thapak.shilp2395@gmail.com>
2022-10-27 14:50:37 +0530

    Added two buttons for two analysis

--- ui/urls.py
+++ ui/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('dashboard', views.dashboard, name="dashboard"),
+    path('sentiment_analysis', views.sentiment_analysis, name="sentiment_analysis"),
+    path('emotional_traits', views.emotional_traits, name="emotional_traits")
 ]
--- ui/views.py
+++ ui/views.py
@@ -5,11 +5,13 @@ import pandas as pd
 import io
 import os
 from expertai.nlapi.cloud.client import ExpertAiClient
+import ast
 
 
 # Create your views here.
 def index(request):
     return render(request, 'ui/index.html')
+
 
 def dashboard(request):
     if request.method =='GET':
@@ -32,19 +34,23 @@ def dashboard(request):
     if len(df.columns) != 1:
         message = "Please upload a file with one column only!"
         return render(request,'ui/index.html', {"message": message})
-    df.columns = ["text"]
-    df = sentiment(df)
+    df.columns = ["Text"]
+    # df = sentiment(df)
     data = df.to_dict('split')
     return render(request,'ui/dashboard.html', {"data": data})
 
-def sentiment(df):
+
+def sentiment_analysis(request):
+    data = ast.literal_eval(data)
+    df = pd.DataFrame(data["data"])
+    df.columns = ['Text']
     os.environ["EAI_USERNAME"] = 'sudurima.banerjee@rakuten.com'
     os.environ["EAI_PASSWORD"] = 'Rima@280'
     client = ExpertAiClient()
 
     list = []
     for i in range(0,len(df)):
-        text=df['text'].iloc[i]
+        text = df['Text'].iloc[i]
         language= 'en'
         document = client.specific_resource_analysis(
         body={"document": {"text": text}}, 
@@ -52,4 +58,35 @@ def sentiment(df):
         list.append(document.sentiment.overall)
 
     df['Sentiment score'] = list
-    return df
+    data = df.to_dict('split')
+    return render(request,'ui/sentiment_analysis.html', {"data": data})
+
+
+def emotional_traits(request):
+    data = request.POST.get('data')
+    data = ast.literal_eval(data)
+    df = pd.DataFrame(data["data"])
+    df.columns = ['Text']
+    os.environ["EAI_USERNAME"] = 'sudurima.banerjee@rakuten.com'
+    os.environ["EAI_PASSWORD"] = 'Rima@280'
+    client = ExpertAiClient()
+
+    list2=[]
+    for i in range(0,len(df["Text"])):
+        text=df["Text"].iloc[i]
+        taxonomy='emotional-traits'
+        document = client.classification(body={"document": {"text": text}}, params={'taxonomy': taxonomy,'language': 'en'})
+
+        categories = []
+        scores = []
+        for category in document.categories:
+            categories.append(category.label)
+            scores.append(category.frequency)
+            
+        if len(categories) == 0:
+            list2.append('NA')
+        else:
+            list2.append(categories[0])
+    df["traits"] = list2
+    data = df.to_dict('split')
+    return render(request,'ui/emotional_traits.html', {"data": data})
